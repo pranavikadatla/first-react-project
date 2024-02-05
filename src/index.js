156f13a1b5cd9b352ef8a2673a47098600f12e97
pranavikadatla <datlapranavika205@gmail.com>
2024-02-05 21:35:53 +0530

    my first project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+const Hello=()=>{
+    let h1Style={color:"pink"}
+    return(
+    <h1 style={h1Style}>Hello World </h1>
+    )
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+}
+
+
+ReactDOM.render(<Hello/>,document.getElementById("root"));
